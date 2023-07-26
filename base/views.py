18e348f01198d06e2c7f7d4d6c171892533cc683
Aneeb <aneebpaloth74@gmail.com>
2023-07-26 03:28:04 -0700

    data added

--- base/views.py
+++ base/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.utils import timezone
+from base.models import Todo
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
 def home(request):
-    return render(request,'base/index.html')
+    todo_list = Todo.objects.all().order_by('-addedtime')
+    return render(request,'base/index.html',{
+        'todo_list' : todo_list
+    })
+
+def add_todo(request):
+    content = request.POST['content']
+    current_time = timezone.now()
+    Todo.objects.create(addedtime = current_time,text = content)
+    
+   
+    return HttpResponseRedirect('/')
